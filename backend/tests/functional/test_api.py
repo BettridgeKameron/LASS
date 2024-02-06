a481043a5a809954f24ced914156a36944a1de5c
Kameron Bettridge <kbettridge@nevada.unr.edu>
2024-02-06 02:31:17 -0800

    Removed duplicate code

--- backend/tests/functional/test_api.py
+++ backend/tests/functional/test_api.py
@@ -7,18 +7,6 @@ import pytest  # https://docs.pytest.org/en/8.0.x/
 # Good resource to check out: https://testdriven.io/blog/flask-pytest/
 
 
-def test_rev_str():
-    """Example of importing a function to do a unit test"""
-    from app import rev_str
-
-    assert rev_str("hello") == "olleh", "Should reverse a normal string"
-    assert rev_str("") == "", "Should handle empty string"
-    assert rev_str("12345") == "54321", "Should reverse numeric strings"
-    assert (
-        rev_str("hello, hi!") == "!ih ,olleh"
-    ), "Should reverse strings with punctuation and spaces"
-
-
 def test_reverse_string(test_client):
     """Functional Test Example of checking the reverse_string endpoint"""
     test_string = "321CbA"
