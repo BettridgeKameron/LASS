9de26097bd05fcedf67bc99dc314270bba3ad09b
Leung-Alvin <89597642+Leung-Alvin@users.noreply.github.com>
2024-02-06 23:35:15 -0800

    removing work from repo
    For privacy sake, I am moving all the work I am doing until I can push the whole thing due to private files.

--- alvin_work/stylomod.py
+++
@@ -1,9 +0,0 @@
-from spellchecker import SpellChecker
-
-def num_errors(text):
-    total = len(text.split())
-    if total == 0:
-        return 0
-    checker = SpellChecker()
-    amount_miss = len(checker.unknown(text.split()))
-    return amount_miss / total
