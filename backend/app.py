4d94023db41987837921fe7e26a30e2e22986ba0
2012-Pai-Raymond <rpai@nevada.unr.edu>
2024-02-26 22:21:12 -0800

    StringInput.vue is Sentiment Analysis

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 
+#Code block below marks the words "the"
+#------------------------
 def mark_the_words(s: str) -> list:
     words = s.split()
-    marked_string = ' '.join([f'<span style="background-color: rgb(255, 255, 128); ">{word}</span>' if word.lower() == 'the' else word for word in words])
+    marked_string = ' '.join([f'<span style="background-color: #66ff00; ">{word}</span>' if word.lower() == 'the' else word for word in words])
     return marked_string
 
 @app.route("/mark_the_words", methods=["POST"])
@@ -17,7 +19,7 @@ def mark_the_words_endpoint():
     string_to_mark = data.get("string", "")
     marked_string = mark_the_words(string_to_mark)
     return jsonify({"marked_string": marked_string})
-
+#------------------------
 def rev_str(s: str) -> str:
     """This is a sample util to demonstrate how to do unit testing in tests/unit/test_utils.py"""
     return s[::-1]
