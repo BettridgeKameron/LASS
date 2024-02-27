d8c779a33cef736b09ffbdbb28c61cb259009119
2012-Pai-Raymond <rpai@nevada.unr.edu>
2024-02-27 00:05:33 -0800

    Added Sentiment Analaysis

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,17 @@ def rev_str(s: str) -> str:
     """This is a sample util to demonstrate how to do unit testing in tests/unit/test_utils.py"""
     return s[::-1]
 
+#Code block below marks the words "the"
+#------------------------
+def sent_analysis(s: str) -> list:
+    words = s.split()
+    sentiment_result = 0
+    marked_string = ' '.join([f'<span style="background-color: #66ff00; ">{word}</span>' if word.lower() == 'the' else word for word in words])
+    sentiment_result += 0.8 * s.lower().count('the')
+    print(sentiment_result)
+    return marked_string
+
+#------------------------
 
 @app.route("/reverse_string", methods=["POST"])
 def reverse_string():
@@ -41,7 +52,10 @@ def obfuscate():
 
 @app.route("/api/v1/text/sentiment-analysis", methods=["POST"])
 def sentiment_analysis():
-    return request.json
+    data = request.json
+    string_to_mark = data.get("string", "")
+    sentiment_result = sent_analysis(string_to_mark)
+    return jsonify({"sentiment_result": sentiment_result})
 
 
 @app.route("/api/v1/text/predict-user-attributes", methods=["POST"])
