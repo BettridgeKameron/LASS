e2c0d3c64fc3bad57e856a95b21a198c8ea4ad6c
2012-Pai-Raymond <rpai@nevada.unr.edu>
2024-03-04 14:43:21 -0800

    reformatted code

--- frontend/src/components/__tests__/SentimentAnalysis.spec.ts
+++ frontend/src/components/__tests__/SentimentAnalysis.spec.ts
@@ -1,38 +1,37 @@
-import { describe, it, expect, vi} from 'vitest';
-import SentAnalyze from '@/views/SentimentAnalysis.vue';
-import { mount, shallowMount} from '@vue/test-utils';
+import { describe, it, expect, vi } from 'vitest'
+import SentAnalyze from '@/views/SentimentAnalysis.vue'
+import { mount, shallowMount } from '@vue/test-utils'
 
 describe('SentimentAnalysis.vue', () => {
   it('should initially display an empty text area', () => {
     const wrapper = shallowMount(SentAnalyze)
     const textarea = wrapper.find('textarea')
     expect(textarea.element.value).toBe('')
-  });
+  })
 
   it('should call the analyzeSentiment method when the button is clicked', async () => {
-    const wrapper = mount(SentAnalyze);
+    const wrapper = mount(SentAnalyze)
 
+    const button = wrapper.find('button')
 
-    const button = wrapper.find('button');
+    const mockAnalyzeSentiment = vi.fn()
+    wrapper.vm.analyzeSentiment = mockAnalyzeSentiment
 
-    const mockAnalyzeSentiment = vi.fn();
-    wrapper.vm.analyzeSentiment = mockAnalyzeSentiment; 
+    await button.trigger('click')
 
-    await button.trigger('click');
+    expect(mockAnalyzeSentiment).toHaveBeenCalled()
+  })
 
-    expect(mockAnalyzeSentiment).toHaveBeenCalled();
-  });
-
-  it('textToAnalyze updates on user input', async() => {
+  it('textToAnalyze updates on user input', async () => {
     const wrapper = shallowMount(SentAnalyze)
     const input = wrapper.find('textarea')
     await input.setValue('This is a test sentence.')
     expect(wrapper.vm.textToAnalyze).toBe('This is a test sentence.')
-  });
+  })
 
-  it('analyzeSentiment clears error message before API call', async() => {
+  it('analyzeSentiment clears error message before API call', async () => {
     const wrapper = shallowMount(SentAnalyze, {
-      data(){
+      data() {
         return {
           errorMessage: 'Previous error message'
         }
@@ -40,8 +39,7 @@ describe('SentimentAnalysis.vue', () => {
     })
 
     wrapper.vm.analyzeSentiment()
-    await new Promise(resolve => setTimeout(resolve, 0))
+    await new Promise((resolve) => setTimeout(resolve, 0))
     expect(wrapper.vm.errorMessage).toBe('')
-
-  });
-});
+  })
+})
--- frontend/src/views/TextRephrase.vue
+++ frontend/src/views/TextRephrase.vue
@@ -3,74 +3,73 @@
     <h1 class="text-4xl font-bold mb-2 py-10">Text Rephrase Tool</h1>
     <div class="flex flex-col space-y-4">
       <div class="flex space-x-3">
-
         <textarea
           v-model="textToAnalyze"
           type="text"
-          style="width: 800px; height: 500px;  resize: none; "
+          style="width: 800px; height: 500px; resize: none"
           class="border-2 border-gray-300 p-2 rounded-md"
           placeholder="Input: Enter a string"
         ></textarea>
         <textarea
+          v-model="rephrasedResult"
           readonly
-          style="width: 800px; height: 500px;  resize: none; color: black; "
+          style="width: 800px; height: 500px; resize: none; color: black"
           class="border-2 border-gray-300 p-2 rounded-md bg-neutral-content"
-        >{{ rephrasedResult }}</textarea>
-    </div>
-    <div class="flex justify-center">
-      <button
+        ></textarea>
+      </div>
+      <div class="flex justify-center">
+        <button
           @click="rephraseText"
-          class=" w-1/3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded"
+          class="w-1/3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded"
         >
           Rephrase
         </button>
-    </div>
+      </div>
       <div v-if="errorMessage" class="text-red-500">Error: {{ errorMessage }}</div>
     </div>
   </div>
 </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue'
-  import axios from 'axios'
-  
-  export default defineComponent({
-    name: 'TextRephrase',
-    setup() {
-      const textToAnalyze = ref('')
-      const errorMessage = ref('')
-      const rephrasedResult = ref('')
-  
-      async function rephraseText() {
-        errorMessage.value = '' // Reset error message
-        try {
-          const response = await axios.post('http://localhost:5000/api/v1/text/rephrase', {
-            text: textToAnalyze.value 
-          })
-          rephrasedResult.value = response.data.rephrased_result;
-          console.log('Response: ', response.data);
-        } catch (error) {
-          errorMessage.value = 'Could not connect to the server.'
-          console.error('Error:', error)
-        }
-      }
-  
-      return {
-        textToAnalyze, 
-        rephrasedResult, 
-        rephraseText, 
-        errorMessage 
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'TextRephrase',
+  setup() {
+    const textToAnalyze = ref('')
+    const errorMessage = ref('')
+    const rephrasedResult = ref('')
+
+    async function rephraseText() {
+      errorMessage.value = '' // Reset error message
+      try {
+        const response = await axios.post('http://localhost:5000/api/v1/text/rephrase', {
+          text: textToAnalyze.value
+        })
+        rephrasedResult.value = response.data.rephrased_result
+        console.log('Response: ', response.data)
+      } catch (error) {
+        errorMessage.value = 'Could not connect to the server.'
+        console.error('Error:', error)
       }
     }
-  })
-  </script>
-<style>
-  .custom-textarea {
-    width: 100px;
-    height: 500px;
-    resize: none;
-    color: black;
-    text-align: justify; 
+
+    return {
+      textToAnalyze,
+      rephrasedResult,
+      rephraseText,
+      errorMessage
+    }
   }
+})
+</script>
+<style>
+.custom-textarea {
+  width: 100px;
+  height: 500px;
+  resize: none;
+  color: black;
+  text-align: justify;
+}
 </style>
-  
