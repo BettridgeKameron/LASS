c8a907814b762e3e6edee88e2993086bb576263d
Zachary Wilhite <z12aggie@gmail.com>
2024-03-04 23:00:33 -0800

    Hotfix 2
    Actually fixed the thing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -43,4 +43,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to) => {
+  document.title = (to.meta?.title as string) ?? 'Default Title'
+})
 export default router
